2c78c1c161ea27395d6d44ffb403c2432138d951
Michael <michaelpspore@gmail.com>
2020-12-13 10:19:46 -0600

    employee setup

---
+++ index.js
@@ -0,0 +1,14 @@
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+const OUTPUT_DIR = path.resolve(__dirname, "output")
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+const render = require("./src/page-template.js.js");
+const teamMembers = [];
+const idArray = [];
+function appMenu() {
+  function createManager() {
+    console.log("Please build your team");
+    inquirer.prompt([
