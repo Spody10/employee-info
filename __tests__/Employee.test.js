291ac4022719ab93fc67a152d611de0e463db56f
Michael <michaelpspore@gmail.com>
2020-12-14 07:43:55 -0600

    classes setup and pass

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -4,26 +4,7 @@ const Employee = require('../lib/Employee');
 test('creates an Employee object', () => {
     const employee = new Employee('John', 123456, 'john@email.com');
     expect(employee.name).toBe('John');
-    expect(employee.id).toEqual(expect.any(Number));
-    expect(employee.email).toEqual(expect.any(String));
+    expect(employee.id).toEqual(123456);
+    expect(employee.email).toBe('john@email.com');
 });
 
-test('returns Employee ID', () => {
-    const employee = new Employee('Jane', 654321, 'jane@email.com')
-    expect(employee.getID()).toBe('654321');
-});
-
-test('returns Employee name', () => {
-    const employee = new Employee('Mike', 369, 'mike@email.com');
-    expect(employee.getName()).toBe('Mike');
-});
-
-test('returns Employee email', () => {
-    const employee = new Employee ('Bob', 250, 'bob@email.com');
-    expect(employee.getEmail()).toBe('Email: bob@email.com');
-});
-
-test('returns Employee role', () => {
-    const employee = new Employee('Liv', 789, 'liv@email.com');
-    expect(employee.getRole()).toBe('Employee');
-})
